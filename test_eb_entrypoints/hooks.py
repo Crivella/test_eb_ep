91bf754615d52f52579f86ed1fa7222e61b85cb8
crivella <davide.grassano@epfl.ch>
2025-06-11 13:14:52 +0200

    Moved hooks to separate file

---
+++ test_eb_entrypoints/hooks.py
@@ -0,0 +1,17 @@
+from easybuild.tools.entrypoints import register_entrypoint_hooks
+from easybuild.tools.hooks import CONFIGURE_STEP, START
+
+
+@register_entrypoint_hooks(START)
+def hello_world():
+    for i in range(5):
+        print("Hello, World! ----------------------------------------")
+
+@register_entrypoint_hooks(CONFIGURE_STEP, pre_step=True)
+def test_pre_configure(*args, **kwargs):
+    print("test_pre_configure called with args:", args, "and kwargs:", kwargs)
+
+@register_entrypoint_hooks(CONFIGURE_STEP, post_step=True)
+def test_post_configure(*args, **kwargs):
+    print("test_post_configure called with args:", args, "and kwargs:", kwargs)
+
