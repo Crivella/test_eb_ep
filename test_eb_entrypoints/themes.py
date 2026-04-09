5ea8a589be9efcd38f453f0117dbe81a9541a76c
crivella <davidecrivella@yahoo.it>
2026-04-09 16:46:17 +0200

    Added example of themes

---
+++ test_eb_entrypoints/themes.py
@@ -0,0 +1,15 @@
+theme1 = {
+    'easybuild.success': 'bold cyan',
+    'easybuild.warning': 'bold yellow',
+    'easybuild.error': 'bold red',
+    'easybuild.prefix1': 'dim hot_pink',
+    'easybuild.prefix1': 'hot_pink',
+}
+
+theme2 = {
+    'easybuild.success': 'dim purple',
+    'easybuild.warning': 'bold yellow',
+    'easybuild.error': 'bold red',
+    'easybuild.prefix1': 'dim orange1',
+    'easybuild.prefix2': 'orange1',
+}
